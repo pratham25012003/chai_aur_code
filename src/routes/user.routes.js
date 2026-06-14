96885e6b41ea8c46993dd6a6117455be80c947db
pratha-hadap <prathamhadap@gmail.com>
2026-06-14 15:28:28 +0530

    add register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js" 
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    // don image ghenar ahhe avatar and coverImage navachi frontend kadun and donhi image 1, 1 ghenar 
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 export default router
